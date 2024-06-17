e2323efc8d5f2101c64232193d6a138005e811a1
BoeingPo <boeing.po@gmail.com>
2024-06-17 23:40:29 +0700

    commit by boeing

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const HomePage = () => {
 
     return(
-        <div style={{display:'flex',justifyContent:'flex-start',textAlign:'start'}}> This is HomePage View Taw is in the respository</div>
+        <div style={{display:'flex',justifyContent:'flex-start',textAlign:'start'}}> This is HomePage View Taw is in the respository
+        <div>ทดสอบการเชื่อม git</div>
+        </div>
     );
 }
 
