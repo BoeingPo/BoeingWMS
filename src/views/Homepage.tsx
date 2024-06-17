c1909f6fdfa57762fb2d551ed45a0db85397f600
agust27 <straw086@gmail.com>
2024-06-17 23:37:08 +0700

    Test commit by taw

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
 
     return(
-        <div style={{display:'flex',justifyContent:'flex-start',textAlign:'start'}}> This is HomePage View</div>
+        <div style={{display:'flex',justifyContent:'flex-start',textAlign:'start'}}> This is HomePage View Taw is in the respository</div>
     );
 }
 
